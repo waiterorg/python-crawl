5364b89b8846379d64a472f32e72eb2a4c47b9f7
waiterorg <iwaiterorg@gmail.com>
2022-08-03 19:13:16 +0430

    Fix base url

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def get_profile_page():
     page = scrape.get_page(url)
     parse_page = scrape.parse_page_as_html(page)
     profile_page = scrape.login(parse_page.form, page.url)
-    links = scrape.get_a_element(profile_page, url.rsplit('/', 0)[0])
+    links = scrape.get_a_element(profile_page, url.rsplit('/', 1)[0])
     result = scrape.result(profile_page, links)
     return result
 
