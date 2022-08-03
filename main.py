fa20fd21bbe88a0c6e1425252cac7ce1e581585e
waiterorg <iwaiterorg@gmail.com>
2022-08-03 18:16:14 +0430

    show page good

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ browser = mechanicalsoup.Browser()
 login_url = "http://olympus.realpython.org/login"
 login_page = browser.get(login_url)
 login_html = login_page.soup
+base_url = "http://olympus.realpython.org"
 
 form = login_html.form
 form.select("input")[0]["value"] = "zeus"
@@ -12,4 +13,10 @@ form.select("input")[1]["value"] = "ThunderDude"
 
 profiles_page = browser.submit(form, login_page.url)
 
-print(profiles_page.soup.title,profiles_page.soup.a)
+print(f"Title : {profiles_page.soup.title.text}")
+
+for link in profiles_page.soup.find_all("a"):
+    link_url = {}
+    link_url[link.text] = base_url + link["href"]
+    print(f"{link.text} : {link_url[link.text]} \n")
+    
