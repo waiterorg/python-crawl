a446cf54f9cd2b047bafea090d9c69162e9ce18e
waiterorg <iwaiterorg@gmail.com>
2022-08-03 17:53:48 +0430

    create parse page asw html and login function for scraping

--- utils/scrape.py
+++ utils/scrape.py
@@ -8,3 +8,14 @@ def get_page(url):
     browser = mechanicalsoup.Browser()
     login_page = browser.get(url)
     return login_page
+
+def parse_page_as_html(page):
+    parse_page = page.soup
+    return parse_page
+
+def login(form, login_page_url):
+    browser = mechanicalsoup.Browser()
+    form.select("input")[0]["value"] = input("Please insert username :")
+    form.select("input")[1]["value"] = input("Please insert password :")
+    profiles_page = browser.submit(form, login_page_url)
+    return profiles_page
